bce6f89b779e388f4c8655ab347d8fa884ebf338
Lawrence Ibarria <lawrence@verdantrobotics.com>
2018-12-23 14:14:22 -0800

    Bazel now uses the image_channel with all includes

--- test/producer.cpp
+++ test/producer.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "image.h"
 
 int main(int argc, char **argv)
 {
+    node_msg::image_channel cn;
     printf("Hello, I am a producer of messages\n");
     return 0;
 }
