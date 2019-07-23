dfc872b6f7205afee0ed62606243d2ac62b66bc2
Chris Leger <chris.leger@verdantrobotics.com>
2019-07-23 23:47:40 +0000

    Simplified API for NodeApp

--- test/producer_app.cpp
+++ test/producer_app.cpp
@@ -8,25 +8,15 @@ using namespace node;
 
 class ProducerNode : public NodeApp {
 public:
-  ProducerNode() {}
-
-  void Connect(const char *topic) {
-    image_channel_ = core_.provides<node_msg::image>(topic);
-
-    node::NodeError res = image_channel_.open();
-    if (res == node::SUCCESS) {
-      SetState(RUNNING);
-    } else {
-      fprintf(stderr, "Failure to create a topic (%d), maybe you need to restart the node server\n", res);
-      SetState(FAILED);
-    }
+  ProducerNode(const std::string topic) {
+    Provides<node_msg::image>(topic);
     usleep(2000000);
   }
 
-  bool HandleNextMessage() override {
+  void Update() override {
     printf(" - Acquiring data (%d)... ", msg_number_);
     fflush(stdout);
-    node_msg::image* img = image_channel_.prepare_message();
+    auto img = PrepareMessage<node_msg::image>();
     printf("Previous value of rows: %d ", img->rows);
     img->rows = msg_number_;
     img->cols = 3+msg_number_;
@@ -36,15 +26,13 @@ public:
     
     printf(" publishing data (%d) ... ", msg_number_);
     fflush(stdout);
-    image_channel_.transmit_message( img );
+    TransmitMessage(img);
     printf(" PUBLISHED!\n");
     usleep(1000000);
     msg_number_++;
     if (msg_number_ == 50) {
       SetState(FINISHED);
     }
-
-    return true;
   }
 
   int msg_number_ = 0;
@@ -59,7 +47,5 @@ std::unique_ptr<NodeApp> node::InitializeNodeApp(int argc, char *argv[],
   }
   printf("Hello, I am a producer of messages. I will publish topic: %s\n", topic );
 
-  auto *producer = new ProducerNode();
-  producer->Connect(topic);
-  return std::unique_ptr<NodeApp>(producer);
+  return std::unique_ptr<NodeApp>(new ProducerNode(topic));
 }
